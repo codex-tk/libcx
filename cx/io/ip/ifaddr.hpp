f0b58aa556c29f32bb6344e9ee336fbb37d0ab21
gwonhyeong <gwonhyeong.tak@gmail.com>
2018-05-29 23:53:02 +0900

    ifaddrs

--- cx/io/ip/ifaddr.hpp
+++ cx/io/ip/ifaddr.hpp
@@ -8,6 +8,10 @@
 #include <vector>
 #include <iostream>
 
+#if CX_PLATFORM != CX_P_WINDOWS
+#include <ifaddrs.h>
+#endif
+
 namespace cx::io::ip {
 
     struct ifaddr {
@@ -18,7 +22,7 @@ namespace cx::io::ip {
     };
 
     std::vector< ifaddr > interfaces( void ) {
-        std::vector< ifaddr > ifaddrs;
+        std::vector< ip::ifaddr > retifaddrs;
 #if CX_PLATFORM == CX_P_WINDOWS 
         // https://msdn.microsoft.com/en-us/library/aa365917.aspx GetAdaptersInfo function
         ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
@@ -41,12 +45,12 @@ namespace cx::io::ip {
                 //case IF_TYPE_IEEE80211: wifi and bluetooth
                     {
                         char buf[1024] = {0 , };
-                        ifaddr addr;
+                        ip::ifaddr addr;
                         addr.name = if_indextoname(pAdapter->Index , buf);
                         addr.ip_address = pAdapter->IpAddressList.IpAddress.String;
                         addr.mask = pAdapter->IpAddressList.IpMask.String;
                         addr.gateway = pAdapter->GatewayList.IpMask.String;
-                        ifaddrs.push_back(addr);
+                        retifaddrs.push_back(addr);
                     }
                     break;
                 case MIB_IF_TYPE_OTHER:
@@ -63,9 +67,53 @@ namespace cx::io::ip {
             }
         }
 #else
+        struct ifaddrs* ifaddrs, *ifa;
+        if ( getifaddrs(&ifaddrs) != -1 ) {
+          ifa = ifaddrs;
+          while( ifa ) {
+            ip::ifaddr addr;
+            addr.name = ifa->ifa_name;
+            addr.ip_address = "";
+            addr.mask = "";
+            addr.gateway = "";
 
+            char host[NI_MAXHOST] = {0 , };
+
+            if ( ifa->ifa_addr ) {
+              int family = ifa->ifa_addr->sa_family;
+              if ( family == AF_INET || family == AF_INET6 ) {
+                if ( getnameinfo( ifa->ifa_addr , 
+                      family == AF_INET 
+                      ? sizeof( struct sockaddr_in )
+                      : sizeof( struct sockaddr_in6 ) ,
+                      host , NI_MAXHOST , nullptr , 0 , NI_NUMERICHOST ) == 0 )
+                {
+                  addr.ip_address = host; 
+                }
+              } 
+            }
+
+
+            if ( ifa->ifa_netmask ) {
+              int family = ifa->ifa_netmask->sa_family;
+              if ( family == AF_INET || family == AF_INET6 ) {
+                if ( getnameinfo( ifa->ifa_netmask, 
+                      family == AF_INET 
+                      ? sizeof( struct sockaddr_in )
+                      : sizeof( struct sockaddr_in6 ) ,
+                      host , NI_MAXHOST , nullptr , 0 , NI_NUMERICHOST ) == 0 )
+                {
+                  addr.mask = host; 
+                }
+              } 
+            }
+            retifaddrs.push_back(addr);
+            ifa = ifa->ifa_next;
+          }
+          freeifaddrs(ifaddrs);
+        }
 #endif
-        return ifaddrs;
+        return retifaddrs;
     }
 
 }
