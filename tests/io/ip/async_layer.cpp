6b2a4a1555f4f192202f387d9a7fa95384b861a4
tk <aoziczero@gmail.com>
2018-06-04 02:05:06 +0900

    -

--- tests/io/ip/async_layer.cpp
+++ tests/io/ip/async_layer.cpp
@@ -16,26 +16,34 @@ TEST( async_layer , connect ) {
     int testValue = 0;
     fd.connect( addresses[0]
         , [&] ( const std::error_code& ec ) {
-            testValue = 1;
+            if ( ec ) 
+                testValue = -1;
+            else
+                testValue = 1;
         });
     ASSERT_EQ( engine.async_layer().run( std::chrono::milliseconds(1000)) , 1);
-    
     ASSERT_EQ( testValue , 1 );
-    auto get = cx::io::ip::basic_async_socket< SOCK_STREAM , IPPROTO_TCP >::buffer_type( "GET / HTTP/1.1\r\n\r\n" );
 
+    auto get = cx::io::ip::basic_async_socket< SOCK_STREAM , IPPROTO_TCP >::buffer_type( "GET / HTTP/1.1\r\n\r\n" );
     fd.write( get , [&] ( const std::error_code& ec , int io_size ){
-        testValue = io_size;
+        if ( ec ) 
+            testValue = -1;
+        else
+            testValue = io_size;
     });
     ASSERT_EQ( engine.async_layer().run( std::chrono::milliseconds(1000)) , 1);
     ASSERT_EQ( testValue , get.len() );
+
     char read_buffer[1024] = { 0 , };
     cx::io::ip::basic_async_socket< SOCK_STREAM , IPPROTO_TCP >::buffer_type rdbuf( read_buffer , 1023 );
-    testValue = 0;
     fd.read( rdbuf , [&] ( const std::error_code& ec , int io_size ){
-        testValue = io_size;
+        if ( ec ) 
+            testValue = -1;
+        else
+            testValue = io_size;
     });
     ASSERT_EQ( engine.async_layer().run( std::chrono::milliseconds(1000)) , 1);
-    ASSERT_TRUE( testValue != 0 );
+    ASSERT_TRUE( testValue > 0 );
 
     gprintf( "Recv %s" , read_buffer );
     fd.close();
